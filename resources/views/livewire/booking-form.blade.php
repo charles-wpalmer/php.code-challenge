a19ec03abf28e2ef4c5ef306ded9c075fd5bfb48
Charles Palmer <charles.palmer@crowdproperty.com>
2024-09-25 13:24:32 +0100

    Move error message into p tag for colour

--- resources/views/livewire/booking-form.blade.php
+++ resources/views/livewire/booking-form.blade.php
@@ -6,19 +6,19 @@
                 <label class="input input-bordered flex items-center gap-2">
                     <input wire:model="title" id="title" type="text" class="grow" placeholder="Title"/>
                 </label>
-                @error('title')<p class="text-sm text-red-600 mt-2"></p>{{ $message }}@enderror
+                @error('title')<p class="text-sm text-red-600 mt-2">{{ $message }}</p>@enderror
             </div>
             <div>
                 <label class="input input-bordered flex items-center gap-2">
                     <input wire:model="start_time" id="start_time" type="datetime-local" class="grow" placeholder="Start date"/>
                 </label>
-                @error('start_time')<p class="text-sm text-red-600 mt-2"></p>{{ $message }}@enderror
+                @error('start_time')<p class="text-sm text-red-600 mt-2">{{ $message }}</p>@enderror
             </div>
             <div>
                 <label class="input input-bordered flex items-center gap-2">
                     <input wire:model="end_time" id="end_time" type="datetime-local" class="grow" placeholder="End date"/>
                 </label>
-                @error('end_time')<p class="text-sm text-red-600 mt-2"></p>{{ $message }}@enderror
+                @error('end_time')<p class="text-sm text-red-600 mt-2">{{ $message }}</p>@enderror
             </div>
         </div>
 
