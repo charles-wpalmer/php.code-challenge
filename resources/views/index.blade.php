024d2a1572e7113caf7c132e0e30dcc22ff84bc7
Charles Palmer <charles.palmer@crowdproperty.com>
2024-09-25 13:37:48 +0100

    Single component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,10 +14,8 @@
 
 <body class="antialiased  h-screen grid bg-slate-100 lg:items-center">
 
-<div class=" grid lg:grid-cols-12 container gap-10 mx-auto p-4 lg:p-8">
-    <livewire:bookings />
     <livewire:booking-form />
-</div>
+
 
 @livewireScripts
 </body>
--- resources/views/livewire/booking-form.blade.php
+++ resources/views/livewire/booking-form.blade.php
@@ -1,3 +1,31 @@
+<div class=" grid lg:grid-cols-12 container gap-10 mx-auto p-4 lg:p-8">
+<div class="lg:col-span-8 py-5 bg-white rounded shadow-md">
+    <h1 class="px-5 text-xl">My Bookings</h1>
+    <div class="overflow-x-auto mt-8">
+        <table class="table table-zebra">
+            <!-- head -->
+            <thead>
+            <tr>
+                <th></th>
+                <th>Booking</th>
+                <th>Start time</th>
+                <th>End time</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            @foreach($bookings as $booking)
+                <tr>
+                    <th>1</th>
+                    <td class="font-medium whitespace-nowrap">{{ $booking->title }}</td>
+                    <td class="whitespace-nowrap">{{ $booking->start_time }}</td>
+                    <td class="whitespace-nowrap">{{ $booking->end_time }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 <div class="lg:col-span-4 p-5 bg-white shadow-md grid gap-6 ">
     <h1 class="text-xl">Add booking</h1>
     <form wire:submit.prevent="createBooking">
@@ -28,7 +56,7 @@
         </div>
     </form>
 </div>
-
+</div>
 <script>
     document.addEventListener('livewire:load', function () {
         flatpickr("#start_time", {
